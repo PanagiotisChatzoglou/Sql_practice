cea4b699e8dd14742d1d3d84a77b707786c83f66
Panagiotis Chatzoglou <panagiotis.chatzoglou@ibm.com>
2025-07-19 00:46:35 +0300

    sql

--- sql1.sql
+++ sql1.sql
@@ -1,4 +1,11 @@
 SELECT 
 	CASE 
-		WHEN replacement_cost
-a
+	WHEN replacement_cost BETWEEN 9.99 AND 19.99
+	THEN 'low'
+	WHEN replacement_cost BETWEEN 20 AND 24.99
+	THEN 'MEDIUM'
+	ELSE 'HIGH'
+	END as cost_range,
+	COUNT(*)
+FROM film 
+GROUP BY cost_range
